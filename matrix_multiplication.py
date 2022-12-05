edfac142cec14996133a513d5d8a91d9973fea9e
Panov Georgii <panovgeorgij819@gmail.com>
2022-12-05 17:35:05 +0500

    update "matrix_multiplication.py"

--- matrix_multiplication.py
+++ matrix_multiplication.py
@@ -1,36 +1,28 @@
 import sys
 # Умножение матриц
 
-sizeOfMatrix_1 = int(input('Размер матрицы: '))
-sizeOfMatrix_2 = int(input('Размер матрицы: '))
-
-
-def Matrix_is_Square():
-    all_matrix = sizeOfMatrix_1 + sizeOfMatrix_2
-    if all_matrix % 2:
-        valid_matrix = all_matrix % 2
-        print(valid_matrix)
-    else:
-        print('Ваша матрица не квадратная')
-        sys.exit()
-
-
-Matrix_is_Square()
+sizeOfMatrix_1h = int(input('Размер матрицы 1 h: '))
+sizeOfMatrix_1w = int(input('Размер матрицы 1 w: '))
+sizeOfMatrix_2h = int(input('Размер матрицы 2 h: '))
+sizeOfMatrix_2w = int(input('Размер матрицы 2 w: '))
 
 
 A = []
 B = []
 
-for i in range(sizeOfMatrix_1):
-    rows = list(map(int, input('Введите цыфры, которые, будут добавлены  в мтарицу через пробел: ').split()))[:sizeOfMatrix_1]
+for i in range(sizeOfMatrix_1h):
+    rows = list(map(int, input('Введите цыфры, которые, будут добавлены  в мтарицу через пробел: ').split()))
     A.append(rows)
 
-for i in range(sizeOfMatrix_2):
-    rows = list(map(int, input('Введите цыфры, которые, будут добавлены  в мтарицу через пробел: ').split()))[:sizeOfMatrix_2]
+for i in range(sizeOfMatrix_2h):
+    rows = list(map(int, input('Введите цыфры, которые, будут добавлены  в мтарицу через пробел: ').split()))
     B.append(rows)
 
 
 multiResult = [[sum(a * b for a, b in zip(Arow, Bcol)) for Bcol in zip(*B)] for Arow in A]
+
+
 print("The multiplication result of matrix A and B is: ")
 for res in multiResult:
     print(res)
+
